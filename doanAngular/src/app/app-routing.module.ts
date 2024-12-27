305805817ea85cadd3bb35dfb505836b4ca43a8e
Kiệt Lê <kietle06022004@gmail.com>
2024-12-27 15:49:44 +0700

    detail cpn, avatar user

--- doanAngular/src/app/app-routing.module.ts
+++ doanAngular/src/app/app-routing.module.ts
@@ -5,15 +5,18 @@ import { HomeComponent } from './home/home.component';
 // 27/12 Cua them login va signup
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { DetailComponent } from './detail/detail.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  {path: 'detail', component: DetailComponent},
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  {path : '',component: HomeComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- doanAngular/src/app/app.module.ts
+++ doanAngular/src/app/app.module.ts
@@ -9,13 +9,15 @@ import { HomeComponent } from './home/home.component';
 // 27/12 Cua them login va signup
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
