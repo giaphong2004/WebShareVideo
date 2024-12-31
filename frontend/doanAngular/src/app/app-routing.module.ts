fcb344416c88d01eeb9feea7ae41db6b99563cd2
giaphong2004 <2200010465@nttu.edu.vn>
2025-01-01 00:21:06 +0700

    signup&&logout done

--- frontend/doanAngular/src/app/app-routing.module.ts
+++ frontend/doanAngular/src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
-import { NgModule, Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-
-// 27/12 Cua them login va signup
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { DetailComponent } from './detail/detail.component';
@@ -10,7 +8,7 @@ import { DetailComponent } from './detail/detail.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
-  {path: 'detail', component: DetailComponent},
+  { path: 'detail', component: DetailComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
 ];
