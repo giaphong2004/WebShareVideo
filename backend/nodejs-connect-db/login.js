5dfc3a705f2c1bc8facec64ebb3d5a19fe04dfb1
Cua-147 <ngphgnguyen@gmail.com>
2025-01-01 14:09:39 +0700

    ko co j

--- backend/nodejs-connect-db/login.js
+++ backend/nodejs-connect-db/login.js
@@ -8,7 +8,7 @@ const connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : '',
-    database : 'stream_hub'
+    database : 'streamhub_sql'
 });
 
 connection.connect(function(err) {
